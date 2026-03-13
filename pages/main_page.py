c74a459d8f0b244044af1dadeaa740aeb1c79194
alex.mitrohin2016@yandex.ru <aleksey.mitrokhin@osinit.com>
2026-03-13 15:00:14 +0300

    Second commit

--- pages/main_page.py
+++ pages/main_page.py
@@ -2,7 +2,6 @@ import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
 from base.base_class import Base
 
 class MainPage(Base):
